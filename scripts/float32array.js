9a122d914116156280b734bf5f0331629f43a965
olivernyc <oliver@static.network>
2019-06-01 21:15:40 -0400

    Non indexed buffers

--- scripts/float32array.js
+++ scripts/float32array.js
@@ -1,47 +1,110 @@
 const lineReader = require("line-reader");
 var fs = require("fs");
+const earcut = require("earcut");
+const ProgressBar = require("./ProgressBar");
 
-var writeStream = fs.createWriteStream("./buildings.dat");
+try {
+	fs.unlinkSync("faces.buffer");
+	fs.unlinkSync("vertices.buffer");
+} catch (err) {}
+let verticesWriteStream = fs.createWriteStream("./buffers/vertices0.buffer");
+let facesWriteStream = fs.createWriteStream("./buffers/faces0.buffer");
 
-const points = [];
-let count = 0;
-lineReader.eachLine("./data/buildings.json", function(line, last) {
-	count += 1;
-	if (count % 10000 === 0) {
-		console.log(count);
-	}
+const vertexMap = new Map();
+let currentVertex = 0;
+let currentTile = 0;
 
-	const building = JSON.parse(line.substring(0, line.length - 1)); // trailing comma
-	addPoints(building);
+let facesCount = 0;
+let vertexCount = 0;
 
-	if (count >= 100000 || last) {
-		//prepare the length of the buffer to 4 bytes per float
-		var buffer = new Buffer(points.length * 4);
-		for (var i = 0; i < points.length; i++) {
-			//write the float in Little-Endian and move the offset
-			buffer.writeFloatLE(points[i], i * 4);
+const TILE_SIZE = 256000;
+const LIMIT = 1083437;
+
+processBuildings();
+
+function processBuildings() {
+	let count = 0;
+	const bar = new ProgressBar(LIMIT);
+	bar.render();
+	lineReader.eachLine("./data/buildings.json", function(line, last) {
+		if (count % 1000 === 0) {
+			bar.curr = count;
+			bar.render();
+		}
+		count += 1;
+
+		if (currentVertex > 0 && currentVertex > TILE_SIZE) {
+			currentTile++;
+			currentVertex = 0;
+			facesCount = 0;
+			vertexMap.clear();
+			verticesWriteStream = fs.createWriteStream(
+				`./buffers/vertices${currentTile}.buffer`
+			);
+			facesWriteStream = fs.createWriteStream(
+				`./buffers/faces${currentTile}.buffer`
+			);
 		}
 
-		fs.writeFileSync(`./data${count}.data`, buffer);
-		console.log(`${count} buildings`);
-		return false;
-	}
-});
+		const building = JSON.parse(line.substring(0, line.length - 1)); // trailing comma
+		processBuilding(building);
 
-function addPoints(building) {
-	const dupePoints = {};
-	building.forEach((surface, index) => {
-		for (var i = 0; i < surface.length; i++) {
-			const point = pointFromVertex(surface[i]);
-			const key = point.join(",");
-			if (dupePoints[key]) continue;
-			points.push(...pointFromVertex(surface[i]));
-			dupePoints[key] = true;
+		if (last) {
+			return false;
 		}
 	});
 }
 
-function pointFromVertex(vertex) {
+function processBuilding(building) {
+	building.forEach((rawFace, index) => {
+		// Remove dupe last point
+		const face = rawFace.slice(0, rawFace.length - 1);
+
+		const vertices = face.reduce((acc, cur) => {
+			acc.push(...cur);
+			return acc;
+		}, []);
+
+		const triangles =
+			face.length === 3
+				? [0, 1, 2]
+				: face.length === 4
+				? [0, 1, 2, 0, 2, 3]
+				: earcut(vertices, null, 3);
+
+		const triangleIndices = triangles.map(i => {
+			return vertexIndex(face[parseInt(i)]);
+		});
+
+		if (!triangleIndices.length) {
+			console.log("Bad triangles", rawFace, vertices);
+		}
+
+		facesWriteStream.write(
+			Buffer.from(Uint32Array.from(triangleIndices).buffer)
+		);
+		facesCount += triangleIndices.length;
+	});
+}
+
+function vertexIndex(rawVertex) {
+	const vertex = scaleVertex(rawVertex);
+	const key = vertexKey(vertex);
+	if (!vertexMap.has(key)) {
+		vertexMap.set(key, currentVertex);
+		verticesWriteStream.write(
+			Buffer.from(Float32Array.from(vertex).buffer)
+		);
+		currentVertex++;
+	}
+	return vertexMap.get(key);
+}
+
+function vertexKey(vertex) {
+	return vertex.map(float => float.toFixed(1)).join(";");
+}
+
+function scaleVertex(vertex) {
 	return [
 		mapLinear(vertex[1], 194479, 259992, 0, 65513) - 65513 / 2,
 		mapLinear(vertex[2], -39.0158999999985, 1797.1066, 0, 1836),
